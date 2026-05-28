86f5de0b584eccca64d2283e8a34c81932d86626
Tere <teresaolivaresrueda@gmail.com>
2026-05-28 12:07:54 -0400

    estilado

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/**
+ * 🌦️ Al cargar la página esta debe mostrar una alerta con la leyenda “Cargando Reporte del Clima”.
+ * ✔️ En la parte inferior el banner de cookies debe desaparecer al darle clic en aceptar.
+ * 🌡️ Al seleccionar una ciudad la temperatura cambia.
+ */
